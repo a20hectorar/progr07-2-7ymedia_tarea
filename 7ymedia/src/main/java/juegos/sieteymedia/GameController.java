2db44f16c1ed4ba3e334b4ffc9ead166c2277614
Hector <a20hectorar@iessanclemente.net>
2023-03-04 18:36:33 +0100

    implementando la generacion de carta aleatoria(inacabado)

--- 7ymedia/src/main/java/juegos/sieteymedia/GameController.java
+++ 7ymedia/src/main/java/juegos/sieteymedia/GameController.java
@@ -3,8 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package juegos.sieteymedia;
-
+import java.util.Random;
 import juegos.recursos.Baraja;
+import juegos.recursos.Carta;
 
 /**
  *
@@ -14,9 +15,35 @@ public class GameController {
     private Baraja baraja;
     private Jugador jugador;
     
-    public static void main GameController(String[] args){
-        public void juegaMano(int valorCarta){
-        }
+    
+    
+    public static void main(String[] args){
+        int valores[]={1,2,3,4,5,6,7,10,11,12};
+        String paloBaraja[]={"Espadas","Oros","Copas","Bastos"};
+        
+        String palo=paloAleatorio(paloBaraja[]);
+        int valor=valorAleatorio(valores[]);
+        
+        Carta otraCarta=new Carta(palo,valor);
     }
-
+    
+    public void juegaMano(int valorCarta){
+        }
+    
+    
+    public String paloAleatorio(String array[]){
+        int tamañoArray=array.length;
+        Random random=new Random();
+        int indiceArray=random.nextInt(tamañoArray);
+        return array[indiceArray];
+    }
+    
+    public int valorAleatorio(int array[]){
+        int tamañoArray=array.length;
+        Random random=new Random();
+        int indiceArray=random.nextInt(tamañoArray);
+        return array[indiceArray];
+    }
+    
+       
 }
--- 7ymedia/src/main/java/juegos/sieteymedia/Jugador.java
+++ 7ymedia/src/main/java/juegos/sieteymedia/Jugador.java
@@ -4,7 +4,7 @@
  */
 package juegos.sieteymedia;
 
-import juegos.recursos;
+
 import juegos.recursos.Carta;
 
 /**
